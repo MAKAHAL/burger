68d483d282b6ce3f285a8b738aee3a0467af1685
Makah <mecca-131@hotmail.com>
2020-03-04 15:05:49 -0600

    files

--- config/orm.js
+++ config/orm.js
@@ -3,6 +3,7 @@
 
 var connection = require("./connection.js");
 
+
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -13,29 +14,50 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-  // column1=value, column2=value2,...
   var arr = [];
 
+  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
-    arr.push(key + "=" + ob[key]);
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Cheese Burg => 'Cheese Burg')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {burger_name: 'Cheese Burg'} => ["burger_name='Cheese Burg'"]
+      // e.g. {devoured: true} => ["devoured=true"]
+      arr.push(key + "=" + value);
+    }
   }
 
+  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
+///////////////end functions/////////////////////
+
+// Object for all our SQL statement functions.
 var orm = {
+  //selectAll()
   all: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
-      cb(result);
+      cb(result); //do something with the returned sql query result
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+
+/*
+cb = function(res) {
+      cb(res); //second layer function
+    }*/
+
+  //insertOne()
   create: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -52,11 +74,12 @@ var orm = {
       if (err) {
         throw err;
       }
-      cb(result);
+
+      cb(result); //do something with the returned sql query result
     });
   },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
+  //updateOne()
+  // An example of objColVals would be {burger_name: delicious, devoured: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -70,9 +93,11 @@ var orm = {
       if (err) {
         throw err;
       }
-      cb(result);
+
+      cb(result); //do something with the returned sql query result
     });
   }
 };
 
+// Export the orm object for the model (burgers.js).
 module.exports = orm;
