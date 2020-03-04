c607105029f3e2291372e9f7ac02ff2207370e6c
Makah <mecca-131@hotmail.com>
2020-03-04 15:41:39 -0600

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+// var bodyParser = require("body-parser");
+// var methodOverride = require("method-override");
 
 var PORT = process.env.PORT || 3000;
 
@@ -9,8 +9,9 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
@@ -20,10 +21,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
-
-
-        app.listen(PORT, () => {
-            console.log(`App listening on port ${PORT}`);
-        
-    });
+app.use(routes);
+app.listen(PORT, function() {
+        console.log("Listening on port:%s", PORT);
+      });
