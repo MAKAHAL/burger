51b6a773a3298c54c12bb5cec3a1ea90a6a7990b
Makah <mecca-131@hotmail.com>
2020-03-04 22:53:04 -0600

    jaws coneecttion

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@
 // require("dotenv").config();
 
 var mysql = require("mysql");
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
   
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-};
+
 
 connection.connect(function(err) {
   if (err) {
