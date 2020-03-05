85931f2a5066635f12ee5b93b20f78b12d8df209
Makah <mecca-131@hotmail.com>
2020-03-04 22:59:11 -0600

    server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var exphbs = require("express-handlebars");
 var methodOverride = require("method-override");
 var app = express();
 
@@ -13,7 +13,7 @@ app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
