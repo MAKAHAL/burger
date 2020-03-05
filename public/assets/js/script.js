788f22f8b000b11629b7f6d94189910fcece6247
Makah <mecca-131@hotmail.com>
2020-03-04 19:26:11 -0600

    d

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,36 +1,36 @@
-// $(document).ready(function() {
+$(document).ready(function() {
     
 
-//   // //const app = {
-//   //   devourBurger: function (id) {
-//   //     $.ajax({
-//   //         url: `/api/burger/${id}`,
-//   //         type: "PUT",
-//   //         success: function () {
-//   //             window.location.reload();
-//   //         }
-//   //     })
-//   // },
-//   $(".devour-form").on("submit", function(event) {
-//     event.preventDefault();
+  // //const app = {
+  //   devourBurger: function (id) {
+  //     $.ajax({
+  //         url: `/api/burger/${id}`,
+  //         type: "PUT",
+  //         success: function () {
+  //             window.location.reload();
+  //         }
+  //     })
+  // },
+  $(".devour-form").on("submit", function(event) {
+    event.preventDefault();
 
-//     var burger_id = $(this).children(".burger_id").val();
-//     console.log(burger_id);
-//     $.ajax({
-//       method: "PUT",
-//       url: "/burgers/" + burger_id
-//     }).then(function(data) {
-//       // $("#addBurger").on("click", function (e) {
-//       //   e.preventDefault();
-//       //   const data = {
-//       //       name: $("#burgerName").val()
-//       //   };
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
+    $.ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    }).then(function(data) {
+      // $("#addBurger").on("click", function (e) {
+      //   e.preventDefault();
+      //   const data = {
+      //       name: $("#burgerName").val()
+      //   };
       
-//       location.reload();
-//     });
+      location.reload();
+    });
 
-//   });
+  });
+});
+//$(document).ready(function() {
+//   app.init();
 // });
-// //$(document).ready(function() {
-// //   app.init();
-// // });
