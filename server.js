53959d65451dc43b9465a8f9162efe4fb7c43be3
Makah <mecca-131@hotmail.com>
2020-03-04 19:24:20 -0600

    fix

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
+var methodOverride = require("method-override");
 var app = express();
 
-var port = process.env.PORT || 3000;
-
+// var port = process.env.PORT || 3000;
+var port = 3000;
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(methodOverride("_method"));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
